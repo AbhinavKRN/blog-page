f30dd29b0a4384bb72a7fafc061405129be75c8b
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-09-05 14:46:07 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, Container } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import CategoryPage from './pages/CategoryPage';
+import BlogDetails from './components/BlogDetails';
+import BlogEditor from './components/BlogEditor';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import getTheme from './theme';
+import ThemeSwitcher from './components/ThemeSwitcher';
 
-function App() {
+const App = () => {
+  const themeMode = useSelector((state) => state.blog.theme);
+  const theme = getTheme(themeMode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Container maxWidth="lg">
+          <ThemeSwitcher />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/category/:category" element={<CategoryPage />} />
+            <Route path="/blog/:id" element={<BlogDetails />} />
+            <Route path="/edit/:id" element={<BlogEditor />} />
+            <Route path="/create" element={<BlogEditor />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
