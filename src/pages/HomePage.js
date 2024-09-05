ed0f71fd76e0e6d0a9eaedb89f1a2f165af126a7
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-09-05 14:42:38 +0530

    Home Page added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import BlogList from '../components/BlogList';
+import CategorySidebar from '/components/CategorySidebar';
+import { Grid, Typography, Container } from '@mui/material';
+
+const HomePage = () => {
+  const blogs = useSelector((state) => state.blog.blogs);
+  const selectedCategory = useSelector((state) => state.blog.selectedCategory);
+
+  const filteredBlogs =
+    selectedCategory === 'All'
+      ? blogs
+      : blogs.filter((blog) => blog.category === selectedCategory);
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3}>
+          <CategorySidebar />
+        </Grid>
+
+        <Grid item xs={12} md={9}>
+          {filteredBlogs.length > 0 ? (
+            <BlogList blogs={filteredBlogs} />
+          ) : (
+            <Typography variant="h6" color="textSecondary">
+              No blogs found in this category.
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomePage;
